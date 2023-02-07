e3a8a56207cad056e2fb42938f3bfd19f8770ab0
root <root@DESKTOP-VUKN8Q7.localdomain>
2023-02-06 22:57:50 -0300

    test: tornando string em caixa baixa

--- testes/palindromo/utils/test_limpar_string.py
+++ testes/palindromo/utils/test_limpar_string.py
@@ -1,4 +1,4 @@
-from src.limpar_string.limpa import remover_espacos_em_branco
+from src.limpar_string.limpa import *
 
 def test_remover_espacos():
 
@@ -7,3 +7,11 @@ def test_remover_espacos():
     validacao = remover_espacos_em_branco(texto)
 
     assert (' ' in validacao) == False
+
+def test_caixa_baixa():
+
+    texto = "QUALQUER TEXTO"
+
+    validacao = caixa_baixa(texto)
+
+    assert validacao == "qualquer texto"
