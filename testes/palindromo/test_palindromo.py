a52bf801857230e62a7cc828cbf0b8d05acd35d4
root <root@DESKTOP-VUKN8Q7.localdomain>
2023-02-06 22:25:56 -0300

    feat: quebrando os testes com validação falsa

--- testes/palindromo/test_palindromo.py
+++ testes/palindromo/test_palindromo.py
@@ -2,3 +2,6 @@ from palindrommo.palindromo import valida_palindromo
 
 def test_palindromo_valido():
     assert valida_palindromo("qualquer texto") == True
+
+def test_palindromo_invalido():
+    assert valida_palindromo("qualquer texto") == False
