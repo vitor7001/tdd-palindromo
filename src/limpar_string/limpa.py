4d03429d5f775509384f61d944e9344aa5beeeb6
root <root@DESKTOP-VUKN8Q7.localdomain>
2023-02-06 23:05:25 -0300

    feat: codigo para limpar acentos

--- src/limpar_string/limpa.py
+++ src/limpar_string/limpa.py
@@ -1,5 +1,12 @@
+import unicodedata
+
 def remover_espacos_em_branco(texto):
     return texto.replace(" ", "")
 
 def caixa_baixa(texto):
     return texto.lower()
+
+def remover_acentos(texto):
+    texto = ''.join(ch for ch in unicodedata.normalize('NFKD', texto)
+                        if not unicodedata.combining(ch))
+    return texto
