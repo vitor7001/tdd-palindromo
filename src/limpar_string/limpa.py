29333949b1d2464cfc025cbd904d39cb043ece10
root <root@DESKTOP-VUKN8Q7.localdomain>
2023-02-06 23:11:30 -0300

    feat: removendo acentos das frases

--- src/limpar_string/limpa.py
+++ src/limpar_string/limpa.py
@@ -10,3 +10,11 @@ def remover_acentos_nas_letras(texto):
     texto = ''.join(ch for ch in unicodedata.normalize('NFKD', texto)
                         if not unicodedata.combining(ch))
     return texto
+
+def remover_acentos_na_frase(texto):
+    texto = texto.replace('-', '')
+    texto = texto.replace('.', '')
+    texto = texto.replace('!', '')
+    texto = texto.replace(',', '')
+    texto = texto.replace('/', '')
+    return texto
